3536b481119ff6d4861c88f8c8a4e9d788f97daf
Neeraj Rajeev Shetkar <63545915+ineerajrajeev@users.noreply.github.com>
2022-04-16 18:34:13 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='piNet',
+    version='0.0.2',
+    license='GNU General Public License',
+    author="ineerajrajeev",
+    author_email='neerajshetkar@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ineerajrajeev/pieNet',
+    keywords='python to HTML',
+    install_requires=[],
+
+)
